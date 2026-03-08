5e5f17772b8d668465e15544ade0d2338271e211
Berke Umut Şahintürk <sberkeumutsahinturk@gmail.com>
2026-03-08 15:43:00 +0300

    Refactor code structure for improved readability and maintainability

--- js/matrix_8_ble_web_app.ui.js
+++ js/matrix_8_ble_web_app.ui.js
@@ -50,8 +50,8 @@ const drawPresetEditorState = {
   lastPaintedKey: "",
 };
 const MAX_LOCAL_PRESETS = 64;
-const DRAW_PRESETS_REMOTE_URL = "./presets/draw_presets.json";
-const ANIM_PRESETS_REMOTE_URL = "./presets/anim_presets.json";
+const DRAW_PRESETS_REMOTE_URL = "../presets/draw_presets.json";
+const ANIM_PRESETS_REMOTE_URL = "../presets/anim_presets.json";
 let drawPresetSelectedName = "";
 let remoteDrawPresets = [];
 let remoteAnimPresets = [];
